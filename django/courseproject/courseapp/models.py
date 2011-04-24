902ea80f1c4ff5ebcf3ae45078251f6af6c9ac74
Ian <typppo@gmail.com>
2011-04-23 23:32:48 -0400

    more schema changes

--- django/courseproject/courseapp/models.py
+++ django/courseproject/courseapp/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib import admin
 
 class User(models.Model):
-    id = models.CharField()
+    key = models.CharField(max_length=60)
 
 class Course(models.Model):
-    id = models.CharField()
+    key = models.CharField(max_length=60)
     professors = models.ManyToManyField('Professor')
 
 class CourseOffering(models.Model):
@@ -12,24 +13,33 @@ class CourseOffering(models.Model):
     distrib = models.ManyToManyField('Distrib')
     term = models.ForeignKey('Term')
 
+    crn = models.IntegerField()
     cap = models.IntegerField()
-    location = models.CharField()
+    location = models.CharField(max_length=70)
     lab = models.BooleanField()
     nro = models.BooleanField()
     seminar = models.BooleanField()
     examdate = models.DateField()
 
 class Term(models.Model):
-    id = models.CharField()
+    key = models.CharField(max_length=5)
 
 class Professor(models.Model):
-    id = models.CharField()
-    name = models.CharField()
+    key = models.CharField(max_length=60)
+    name = models.CharField(max_length=60)
     text = models.TextField()
 
 class Review(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=60)
     text = models.TextField()
 
 class Distrib(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=60)
+
+admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(CourseOffering)
+admin.site.register(Term)
+admin.site.register(Professor)
+admin.site.register(Review)
+admin.site.register(Distrib)
